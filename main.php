12b17a576cc688be372a600486f39ee99cffbeec
Aliff Zulhelmi | 0xD0z <90973479+AliffZulhelmi@users.noreply.github.com>
2023-02-06 02:56:40 +0800

    Add files via upload

---
+++ main.php
@@ -0,0 +1,65 @@
+<html lang="en">
+<head> 
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TECHNOSIAST STORE</title>
+    <link rel="stylesheet" href="/style.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
+
+*{
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+}
+nav{
+    width: 100%;
+    height: 50px;
+    line-height: 50px;
+    padding: 0px 100px;
+    background-color: black;
+}
+nav .logo p{
+    font-size: 30px;
+    font-family: 'Roboto', sans-serif;
+    font-weight: bold;
+    float: left;
+    color: white;
+    text-transform: uppercase;
+    letter-spacing: 1.5px;
+    cursor: pointer;
+}
+nav ul{
+    float: right;
+}
+nav li{
+    display: inline-block;
+    list-style: none;
+}
+nav li a{
+    font-size: 18px;
+    font-family: 'Roboto', sans-serif;
+    text-transform: uppercase;
+    padding: 0px 30px;
+    color: white;
+    text-decoration: none;
+}
+body{
+    background-color: #E8E8E8;
+}
+    </style>
+</head>
+<body>
+  <nav>
+    <div class="logo">
+        <p>TECHNOSIAST</p>
+    </div>
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Products</a></li>
+    </ul>
+  </nav>
+    
+</body>
+</html>
